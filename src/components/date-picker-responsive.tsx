0ee9367f2a6cecef1193d4b244c97d13867e02f6
Mateus Sonnenberg <mateus.sonnenberg.amaral@gmail.com>
2025-10-24 00:20:53 +0000

    boa agora ta funcionando, porem o layout do calendario ainda ta meio bug

--- src/components/date-picker-responsive.tsx
+++ src/components/date-picker-responsive.tsx
@@ -64,17 +64,6 @@ export function DatePickerResponsive({ date, setDate, calendarProps = {} }: Date
         initialFocus
         locale={ptBR}
         className={cn(!isDesktop && "w-full border-0")}
-        classNames={{
-          months: cn(!isDesktop && "w-full"),
-          month: cn(!isDesktop && "w-full space-y-6"),
-          table: cn(!isDesktop && "w-full border-separate border-spacing-2"),
-          head_cell: cn(!isDesktop && "w-auto text-lg"),
-          row: cn(!isDesktop && "flex w-full mt-2"),
-          cell: cn(!isDesktop && "h-14 w-14 text-center text-lg p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20"),
-          day: cn("h-14 w-14 p-0 font-normal aria-selected:opacity-100 text-lg", !isDesktop && "h-14 w-14 text-lg"),
-          caption_label: cn(!isDesktop && "text-2xl"),
-          nav_button: cn(!isDesktop && "h-10 w-10"),
-        }}
         {...calendarProps}
       />
   )
@@ -102,7 +91,7 @@ export function DatePickerResponsive({ date, setDate, calendarProps = {} }: Date
           <DrawerTitle>Seletor de Data</DrawerTitle>
           <DrawerDescription>Selecione uma data no calendário.</DrawerDescription>
         </DrawerHeader>
-         <div className="mt-4 flex items-center justify-center min-h-[50vh] p-4">
+         <div className="mt-4 flex items-center justify-center p-4">
           <CalendarComponent />
         </div>
       </DrawerContent>
